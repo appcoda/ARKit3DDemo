a546ef231d0b0bc39abc243085a356f117412c7f
Simon Ng <simonng@appcoda.com>
2017-11-10 18:47:34 +0800

    Initial version

---
+++ ARKit3DDemo/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  AppCoda-ARKit-Part2
+//
+//  Created by Jayven Nhan on 23/10/17.
+//  Copyright © 2017 Jayven Nhan. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureLighting()
+//        addPaperPlane()
+        addCar()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    func addPaperPlane(x: Float = 0, y: Float = 0, z: Float = -0.5) {
+        guard let paperPlaneScene = SCNScene(named: "paperPlane.scn"), let paperPlaneNode = paperPlaneScene.rootNode.childNode(withName: "paperPlane", recursively: true) else { return }
+        paperPlaneNode.position = SCNVector3(x, y, z)
+        sceneView.scene.rootNode.addChildNode(paperPlaneNode)
+    }
+    
+    func configureLighting() {
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
+    }
+    
+    func addCar(x: Float = 0, y: Float = 0, z: Float = -0.5) {
+        guard let carScene = SCNScene(named: "car.dae") else { return }
+        let carNode = SCNNode()
+        let carSceneChildNodes = carScene.rootNode.childNodes
+        for childNode in carSceneChildNodes {
+            carNode.addChildNode(childNode)
+        }
+        carNode.position = SCNVector3(x, y, z)
+        carNode.scale = SCNVector3(0.5, 0.5, 0.5)
+        sceneView.scene.rootNode.addChildNode(carNode)
+    }
+}
